420c43c220ff057f9378ddcc3588b0010a3da9d4
Eltotino <totojaber1@gmail.com>
2021-07-10 02:35:22 +0100

    csv export

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""DataAPI module"""
+import requests
+import sys
+import csv
+
+
+def Csv(employee_id):
+    """ export data to CSV"""
+    employee_id = sys.argv[1]
+    r1 = requests.get("https://jsonplaceholder.typicode.com/users/" + employee_id).json()
+    employee_username = r1.get('username')
+    r2 = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' +
+        (employee_id) + '/todos')
+    with open("{}.csv".format(employee_id), "w") as CSVFile:
+        author = csv.writer(CSVFile, quoting=csv.QUOTE_ALL)
+        for T in r2.json():
+            author.writerow([employee_id, employee_username,
+                            T.get('completed'), T.get('title')])
+
+if __name__ == "__main__":
+    Csv(sys.argv[1])
