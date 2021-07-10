714969e29a9a74485ad4cabe7235eeb80c824f10
Eltotino <totojaber1@gmail.com>
2021-07-10 13:39:05 +0100

    subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,21 +5,21 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	"""number of subscribers for a subreddit"""
-	api_url = 'https://www.reddit.com'
-	query = 'r/{}/about.json'.format(subreddit)
-	headers = {
-	"User-Agent": "linux.hbtn.advanced.api (by u/Disastrous-Mistake72)"
-	}
-	requ = requests.get(
-		url='{}/{}'.format(api_url, query),
-		headers=headers,
-		allow_redirects=False
-	)
-	if requ.status_code == 404:
-		return 0
-	res = requ.json()
-	return res.get('data').get('subscribers')
+    """number of subscribers for a subreddit"""
+    api_url = 'https://www.reddit.com'
+    query = 'r/{}/about.json'.format(subreddit)
+    headers = {
+        "User-Agent": "linux.hbtn.advanced.api (by u/Disastrous-Mistake72)"
+    }
+    requ = requests.get(
+        url='{}/{}'.format(api_url, query),
+        headers=headers,
+        allow_redirects=False
+    )
+    if requ.status_code == 404:
+        return 0
+    res = requ.json()
+    return res.get('data').get('subscribers')
 
 if __name__ == '__main__':
-	number_of_subscribers(subreddit)
+    number_of_subscribers(subreddit)
