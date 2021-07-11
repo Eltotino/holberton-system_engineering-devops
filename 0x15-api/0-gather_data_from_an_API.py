dc7fe26a0944f764a82a8d8f705eefaef8806f97
Eltotino <totojaber1@gmail.com>
2021-07-12 00:01:21 +0100

    gather

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,16 +3,26 @@
 import requests
 import sys
 
-
-if __name__ == '__main__':
-    api_url = 'https://jsonplaceholder.typicode.com'
+def APIdata(employee_id):
+    """ Displays information about the ID number"""
     employee_id = sys.argv[1]
-    r1 = requests.get('{}/users/{}'.format(api_url, employee_id))
-    employee_name = r1.json().get('name')
-    r2 = requests.get('{}/todos?userId={}'.format(api_url, employee_id))
-    responses = r2.json()
-    done_tasks = [t for t in responses if t.get('completed')]
+    Done = 0
+    TasksDone = []
+    all_tasks = 0
+    user_url = 'https://jsonplaceholder.typicode.com/users/' + employee_id
+    r1 = requests.get(user_url).json()
+    employee_name = r1.get('name')
+    r2 = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+    for i in r2:
+        if i.get('userId') == int(employee_id):
+            if i.get('completed'):
+                TasksDone.append(i['title'])
+                Done += 1
+                all_tasks += 1
     print('Employee {} is done with tasks({}/{}):'.
-          format(employee_name, len(done_tasks), len(responses)))
-    for t in done_tasks:
-        print('\t {}'.format(t.get('title')))
+          format(employee_name, Done, all_tasks))
+    for t in TasksDone:
+        print("\t {}".format(t))
+
+if __name__ == "__main__":
+    APIdata(sys.argv[1])
